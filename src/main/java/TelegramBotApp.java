2a3f45eb63dcad04e9449dcb603351720947d6b6
Stas Kotelnykov <staskotelnykov@gmail.com>
2022-09-23 00:45:12 +0300

    myBotMenu

---
+++ src/main/java/TelegramBotApp.java
@@ -0,0 +1,9 @@
+import com.app.feature.telegram.TelegramBotService;
+
+import java.io.IOException;
+
+public class TelegramBotApp {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TelegramBotService botService = new TelegramBotService();
+    }
+}
